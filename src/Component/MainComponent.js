0afbc955f9eb814a5f70fdc315529d401e2f3cc1
tuyetnhi <tuyetnhi.official@gmail.com>
2023-06-17 10:56:36 +0700

    Single Page Applications Part 1

--- src/Component/MainComponent.js
+++ src/Component/MainComponent.js
@@ -1,37 +1,57 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 import Menu from './MenuComponent';
 import DishDetail from './DishDetaiComponent';
 import Footer from './FooterComponent';
 import Header from './HeaderComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+
+import Contact from './ContactComponent';
+import Home from './HomeComponent';
 
 class Main extends Component {
 
   constructor(props) {
     super(props);
+
     this.state = {
-        dishes: DISHES,
-        selectedDish: null
+      dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
+      selectedDish: null
     };
   }
 
   onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
+    this.setState({ selectedDish: dishId });
   }
 
   render() {
+    const HomePage = () => {
+      return (
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
+    };
+
     return (
       <div>
-        
         <Header />
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        <Route exact path='/' component={HomePage} />
+        <Route path='/menu' component={() => <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />} />
+        <Route exact path='/contactus' component={Contact} />
         <Footer />
-
       </div>
     );
   }
-
 }
+
 export default Main;
